7aebe4063960925c703ff0a9200091fbe7825bae
Alan R <alanrobinson1337@gmail.com>
2024-01-11 19:02:25 +0000

    Initial commit

---
+++ warmup/src/main/java/uk/ac/gla/spre/warmup/WarmupApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.gla.spre.warmup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmupApplication.class, args);
+	}
+
+}
---
+++ warmup/src/test/java/uk/ac/gla/spre/warmup/WarmupApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.gla.spre.warmup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
